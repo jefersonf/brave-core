1af6293b75761ee0d4d1f743611b11e5b6159968
Mark Pilgrim <mpilgrim@brave.com>
2021-10-20 12:53:33 -0400

    fix name collision

--- chromium_src/third_party/blink/renderer/modules/speech/speech_synthesis.h
+++ chromium_src/third_party/blink/renderer/modules/speech/speech_synthesis.h
@@ -6,6 +6,10 @@
 #ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_SPEECH_SPEECH_SYNTHESIS_H_
 #define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_SPEECH_SPEECH_SYNTHESIS_H_
 
+// We need to include this here before redefining OnSetVoiceList to avoid a
+// name collision with a method in the mojom class.
+#include "third_party/blink/public/mojom/speech/speech_synthesis.mojom-blink.h"
+
 #define OnSetVoiceList                                                \
   OnSetVoiceList_ChromiumImpl(                                        \
       Vector<mojom::blink::SpeechSynthesisVoicePtr> voices); \
